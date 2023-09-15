39426169a288ebada6931c0245c90ae638012c53
seiya <seiya01311224@gmail.com>
2023-09-15 15:45:08 +0900

    commit

--- public/index.php
+++ public/index.php
@@ -27,7 +27,7 @@
     
     <section class=but>
       <form action="counter.php" method="post">
-        <button type="button" class="call-button">呼び出す</button>
+        <button type="submit" class="call-button">呼び出す</button>
       </form>
     </section>
     
