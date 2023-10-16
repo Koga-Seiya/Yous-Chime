67f75ce62a4cb8d3f457fbf9fd4d23677c1386f9
seiya <seiya01311224@gmail.com>
2023-10-17 00:18:43 +0900

    Yous-Chime

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
+<?php
+try {
+  $dbHost = getenv('DB_HOST');
+  $dbPort = getenv('DB_PORT');
+  $dbName = getenv('DB_NAME');
+  $dbUser = getenv('DB_USER');
+  $dbPass = getenv('DB_PASS');
+  $dsn = "mysql:dbname=$dbName;host=$dbHost:$dbPort";
+  $dbh = new PDO($dsn, $dbUser, $dbPass);
+  //print 'データベース接続成功';
+} catch (PDOException $e) {
+  //echo 'データベース接続失敗:';
+  print $dbName;
+  echo $e->getMessage();
+}
+
+$stmt = $dbh->query('SELECT * from Chime_board');
+$message_length = $stmt->rowCount();
+
+function convertTz($datetime_text)
+{
+  $datetime = new DateTime($datetime_text);
+  $datetime->setTimezone(new DateTimeZone('Asia/Tokyo'));
+  return $datetime->format('Y/m/d H:i:s');
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -21,8 +51,38 @@
 
     <!-- 縦型ナビゲーションバー -->
     <div id='button-list'>
-      
+      <div id='home-button'><a ref='example'>Home</a></div>
+      <form action="map.php" method="post">
+        <div id='map-button' ><button type ="submit" class="fas fa-map">map</button></div>
+      </form>
     </div>
+    
+    <hr class="page-divider" />
+
+      <div class="message-list-cover">
+        <small>
+          <?php echo $message_length;?>件
+        </small>
+        
+        <?php while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {?>
+          <?php $lines = explode("\n",$row['contents']);?>
+          <div class="message-item">
+            <div class="message-title">
+              <div><?php echo htmlspecialchars($row['place'], ENT_QUOTES); ?></div>
+              <small><?php echo convertTz($row['created_at']); ?></small>
+              <div class="spacer"></div>
+              <form action="/" method="post" style="text-align:right">
+                <input type="hidden" name="id" value="<?php echo $row['wait']; ?>" />
+                <input type="hidden" name="action_type" value="delete" />
+                <button type="submit" class="message-delete-button">削除</button>
+              </form>
+            </div>
+            <?php foreach ($lines as $line) { ?>
+              <p class="message-line"><?php echo htmlspecialchars($line, ENT_QUOTES); ?></p>
+            <?php } ?>
+          </div>
+        <?php } ?>
+      </div>
 
     
     
